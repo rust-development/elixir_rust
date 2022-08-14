2f92fbea0f5e5ca9406ac41118534ea71d049527
samick17 <boneache@gmail.com>
2022-08-14 17:47:59 +0800

    1. Added sample code for Elixir Rust NIFs integration

---
+++ lib/elixir_rust.ex
@@ -0,0 +1,21 @@
+defmodule ElixirRust do
+  @moduledoc """
+  Documentation for `ElixirRust`.
+  """
+
+  @doc """
+  Rust integration
+
+  ## Examples
+
+      iex> ElixirRust.add(1, 2)
+      3
+
+  """
+  use Rustler,
+    otp_app: :elixir_rust,
+    crate: :rustlib_add
+
+  def add(_arg1, _arg2), do: :erlang.nif_error(:nif_not_loaded)
+
+end
